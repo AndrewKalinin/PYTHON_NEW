69b6611e160361899459ea52cd18952c88b9ea47
AndrewKalinin <96065838+AndrewKalinin@users.noreply.github.com>
2023-05-31 14:07:00 +0300

    Add files via upload

---
+++ SEM5/task3.py
@@ -0,0 +1,13 @@
+# Создайте функцию генератор чисел Фибоначчи (см. Википедию)
+
+
+def fibona44i(limit: int):
+    fibo = [0, 1]
+    while limit > 0:
+        yield fibo[-1]
+        fibo.append(fibo[-1] + fibo[-2])
+        limit -= 1
+
+
+for number in fibona44i(10):
+    print(number)
